2db0f5fb03445b76488d8415929b46df8598a1e6
Innocent_Bern <innocentbern22@gmail.com>
2022-05-15 19:16:03 +0300

    fetch data from API and display it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import "./Sidebar"
+import { useState,useEffect } from 'react';
+import Sidebar from './Sidebar';
+import useFetch from './useFetch';
 
 function App() {
+  const [contentWidth, setContentWidth]= useState()
+  const { joke, isPending, error}= useFetch("https://v2.jokeapi.dev/joke/Programming?amount=3")
+  console.log( "Pending", isPending, "Error", error );
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="home">
+      <Sidebar setContentWidth={setContentWidth} />
+      <div style={contentWidth} className="content">
+        <h1>I got jokes</h1>
+        {error && <h1>{error}</h1> }
+        {isPending && <h1>Loading....</h1> }
+        {joke && joke.jokes.map((joke)=>(
+           <div key={joke.id}> 
+              {joke.type == "single" ? (
+                <>
+                 <h1> Single: {joke.joke} </h1>
+                </>
+              ) : (<>
+                  <h1>Setup : {joke.setup}</h1>
+                  <h1>Delivery: {joke.delivery}</h1>
+              </>) }
+           </div>
+        ))}
+      </div>
     </div>
+    </>
   );
 }
 
