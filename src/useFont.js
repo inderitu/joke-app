d93b04f69c8c91724e60f018eaf804271d3d7066
Innocent_Bern <innocentbern22@gmail.com>
2022-05-17 18:30:52 +0300

    created hook to manage font style

---
+++ src/useFont.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+const useFont = (pathname) => {
+    const styles= {
+        '/': {fontFamily: "'IBM Plex Mono', monospace"},
+        '/Miscellaneous': {fontFamily: "'Love Ya Like A Sister', cursive"},
+        '/Dark': {fontFamily: "'Rubik Wet Paint', cursive"},
+        '/Pun' : {fontFamily: "'Lakki Reddy', cursive"},
+        '/Christmas': {fontFamily: "'Mountains of Christmas', cursive"},
+        '/Spooky': {fontFamily: "'Creepster', cursive"}
+    }
+
+   return styles[pathname]
+}
+ 
+export default useFont;
