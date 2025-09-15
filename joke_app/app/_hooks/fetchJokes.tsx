52cef1dbe064951a8f03477df12d5a6dc0963abb
inderitu <innocentbern22@gmail.com>
2025-09-15 10:52:37 +0300

    redo

--- joke_app/app/_hooks/fetchJokes.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export async function Fetch_Jokes(category: string) {
-    let url: string = `https://v2.jokeapi.dev/joke/${category}?amount=3`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
